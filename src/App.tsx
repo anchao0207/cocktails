ebc8c8a6ff93e834dee49e94f5e2a9420ee940e5
An Trieu <anchao0207@gmail.com>
2025-07-08 20:20:28 +0000

    implement cocktail section

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,17 @@
 import gsap from "gsap";
-import { ScrollTrigger, SplitText } from "gsap/all";
+import { ScrollSmoother, ScrollTrigger, SplitText } from "gsap/all";
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
+import Cocktails from "./components/Cocktails";
 
-gsap.registerPlugin(ScrollTrigger, SplitText);
+gsap.registerPlugin(ScrollTrigger, SplitText, ScrollSmoother);
 
 const App = () => {
   return (
     <main>
       <Navbar />
       <Hero />
-      <div className="h-dvh bg-black"></div>
+      <Cocktails />
     </main>
   );
 };
