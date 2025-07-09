64788531960bd09515900f64a8955c16f4305352
An Trieu <anchao0207@gmail.com>
2025-07-09 21:59:03 +0000

    implement about section

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { ScrollSmoother, ScrollTrigger, SplitText } from "gsap/all";
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
 import Cocktails from "./components/Cocktails";
+import About from "./components/About";
 
 gsap.registerPlugin(ScrollTrigger, SplitText, ScrollSmoother);
 
@@ -12,6 +13,7 @@ const App = () => {
       <Navbar />
       <Hero />
       <Cocktails />
+      <About />
     </main>
   );
 };
